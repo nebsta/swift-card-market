a66110f03fb7894ac6c4529ef1296a8e98ba90be
nebsta <ben.nat.wallis@gmail.com>
2021-11-14 13:26:58 +0100

    setting up basic xcode project

---
+++ swift-card-market/swift_card_marketApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_card_marketApp.swift
+//  swift-card-market
+//
+//  Created by Benjamin Wallis on 14/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct swift_card_marketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
